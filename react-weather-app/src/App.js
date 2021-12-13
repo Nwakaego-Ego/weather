2e408bac8931a94d64cb36aeaf52f443db1fbb2f
Nwakaego-Ego <81041326+Nwakaego-Ego@users.noreply.github.com>
2021-12-13 19:17:24 +0100

    I added Github link

--- react-weather-app/src/App.js
+++ react-weather-app/src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div className="App">
       <h1>Weather Project</h1>
+      <footer>
+        This project was created by Nwakaego Glory Nwaekpe and it is {""}
+      <a href='https://github.com/Nwakaego-Ego/weather'>Open Sourced on Github</a>
+     </footer>
     </div>
   );
 }
