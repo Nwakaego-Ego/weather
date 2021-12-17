926a16ea26741fb5657a66c277082440bca84a71
Nwakaego-Ego <81041326+Nwakaego-Ego@users.noreply.github.com>
2021-12-17 17:56:02 +0100

    Added CSS to the weather app

--- react-weather-app/src/Weather.js
+++ react-weather-app/src/Weather.js
@@ -6,14 +6,14 @@ export default function Weather () {
             <div className="row">
              <div className="col-9">
                 <input type="search" placeholder="Enter city here...."
-                className="form-control"/>
+                className="form-control p-2" autoFocus="on"/>
              </div>
              <div className="col-3">
-                <button className="btn btn-danger">Search</button>
+                <button className="btn btn-danger w-100 p-2">Search</button>
             </div>
          </div>
             <h1>Nigeria</h1>
-            <ul>
+            <ul className="date">
                 <li>Saturday 23:24</li>
                 <li>Partly Cloudy</li>
             </ul>
@@ -21,7 +21,9 @@ export default function Weather () {
                 <div className="col-6">
                     <img 
                         src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-                        />  87F
+                        /> 
+                         <span className="degree">87</span> 
+                        <span className="farenheit">°F</span>
                 </div>
                 <div className="col-6">
                     <ul>
