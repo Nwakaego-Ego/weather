f9c0b2914b32874da4d9e7079bf05291abb4d458
Nwakaego-Ego <81041326+Nwakaego-Ego@users.noreply.github.com>
2021-12-20 13:20:54 +0100

    Updated WeatherIcon.js

--- react-weather-app/src/WeatherIcon.js
+++ react-weather-app/src/WeatherIcon.js
@@ -3,11 +3,6 @@ import ReactAnimatedWeather from 'react-animated-weather';
  
 export default function WeatherIcon() {
     return (
-            <ReactAnimatedWeather
-              icon="CLEAR_DAY"
-              color="red"
-              size={64}
-              animate={true}
-            />
+           
     )
 }
